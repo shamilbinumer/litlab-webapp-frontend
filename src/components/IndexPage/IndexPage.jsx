290fe6647e344e53f4fe42314bcc76fa3570c988
shamil <shamilmohd418@gmail.com>
2025-02-05 10:19:29 +0530

    Home Page 80% Completed

--- src/components/IndexPage/IndexPage.jsx
+++ src/components/IndexPage/IndexPage.jsx
@@ -1,7 +1,13 @@
+import { IoSearchOutline } from 'react-icons/io5'
 import SideNave from '../SideNav/SideNave'
 import './IndexPage.scss'
+import { useState } from 'react'
+import { IoIosHeartEmpty } from 'react-icons/io'
+import { FaFileInvoiceDollar } from 'react-icons/fa'
+import UserProfile from '../UserProfile/UserProfile'
 
 const IndexPage = () => {
+    const [activePaperType,setActivePaperType]=useState('major')
     return (
         <div className='IndexPageMainWrapper'>
             <div className="home-main">
@@ -9,7 +15,149 @@ const IndexPage = () => {
                     <SideNave />
                 </div>
                 <div className="home-right">
-                    right
+                   <UserProfile/>
+                    <h3 className='welcome-heading'>Welcome to <span>LitLab</span></h3>
+                    <h1 className='main-heading'>What you want to learn today?</h1>
+                    <div className="search-bar">
+                        <IoSearchOutline className='search-icon' />
+                        <input type="text" placeholder='Search Live Classes, Recorded, Modules' />
+                    </div>
+                    <div className="paper-types-wrapper">
+                        <div className={`paper-type ${activePaperType=='major'?"active-paper-type":''}`} onClick={()=>setActivePaperType('major')}>
+                            <span>Major</span>
+                            <div className="icon-wrapper">
+                                <img src="/Images/major.png" alt="" />
+                            </div>
+                        </div>
+                        <div className={`paper-type ${activePaperType=='minor'?"active-paper-type":''}`} onClick={()=>setActivePaperType('minor')}>
+                            <span>Minor</span>
+                            <div className="icon-wrapper">
+                                <img src="/Images/minor.png" alt="" />
+                            </div>
+                        </div>
+                   
+                        <div className={`paper-type ${activePaperType=='common'?"active-paper-type":''}`}
+                        onClick={()=>setActivePaperType('common')}>
+                            <span>Common</span>
+                            <div className="icon-wrapper">
+                                <img src="/Images/common.png" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="paper-cards-section">
+                        {activePaperType=='major'&&(
+                            <>
+                            <div className="cards-main row">
+                                <div className="col-lg-4">
+                                    <div className="paper-card">
+                                        <IoIosHeartEmpty className='heart-icon' />
+                                        <img src="/Images/Group 1000004522.png" alt="" className='paper-image' />
+                                        <div className="lesson-container">
+                                            <div className="lesson-icon">
+                                            <FaFileInvoiceDollar className='icon' />
+                                            </div>
+                                            <span>7 Lessons</span>
+                                        </div>
+                                        <h1 className="paper-title">Budget Analysis</h1>
+                                        <p className="paper-description">Budget Analysis examines income, expenses, and goals for efficient resource allocation.</p>
+                                        <button>Learn Now</button>
+                                    </div>
+                                </div>
+                                <div className="col-lg-4">
+                                    <div className="paper-card">
+                                        <IoIosHeartEmpty className='heart-icon' />
+                                        <img src="/Images/Group 1000004522.png" alt="" className='paper-image' />
+                                        <div className="lesson-container">
+                                            <div className="lesson-icon">
+                                            <FaFileInvoiceDollar className='icon' />
+                                            </div>
+                                            <span>7 Lessons</span>
+                                        </div>
+                                        <h1 className="paper-title">Budget Analysis</h1>
+                                        <p className="paper-description">Budget Analysis examines income, expenses, and goals for efficient resource allocation.</p>
+                                        <button>Learn Now</button>
+                                    </div>
+                                </div>
+                                <div className="col-lg-4">
+                                    <div className="paper-card">
+                                        <IoIosHeartEmpty className='heart-icon' />
+                                        <img src="/Images/Group 1000004522.png" alt="" className='paper-image' />
+                                        <div className="lesson-container">
+                                            <div className="lesson-icon">
+                                            <FaFileInvoiceDollar className='icon' />
+                                            </div>
+                                            <span>7 Lessons</span>
+                                        </div>
+                                        <h1 className="paper-title">Budget Analysis</h1>
+                                        <p className="paper-description">Budget Analysis examines income, expenses, and goals for efficient resource allocation.</p>
+                                        <button>Learn Now</button>
+                                    </div>
+                                </div>
+                                <div className="col-lg-4">
+                                    <div className="paper-card">
+                                        <IoIosHeartEmpty className='heart-icon' />
+                                        <img src="/Images/Group 1000004522.png" alt="" className='paper-image' />
+                                        <div className="lesson-container">
+                                            <div className="lesson-icon">
+                                            <FaFileInvoiceDollar className='icon' />
+                                            </div>
+                                            <span>7 Lessons</span>
+                                        </div>
+                                        <h1 className="paper-title">Budget Analysis</h1>
+                                        <p className="paper-description">Budget Analysis examines income, expenses, and goals for efficient resource allocation.</p>
+                                        <button>Learn Now</button>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            </>
+                        )}
+                         {activePaperType=='minor'&&(
+                            <>
+                             <div className="cards-main row">
+                                <div className="col-lg-4">
+                                    <div className="paper-card">
+                                        <IoIosHeartEmpty className='heart-icon' />
+                                        <img src="/Images/Group 1000004522.png" alt="" className='paper-image' />
+                                        <div className="lesson-container">
+                                            <div className="lesson-icon">
+                                            <FaFileInvoiceDollar className='icon' />
+                                            </div>
+                                            <span>7 Lessons</span>
+                                        </div>
+                                        <h1 className="paper-title">Minor Paper</h1>
+                                        <p className="paper-description">Budget Analysis examines income, expenses, and goals for efficient resource allocation.</p>
+                                        <button>Learn Now</button>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            </>
+                        )}
+                        
+                         {activePaperType=='common'&&(
+                            <>
+                              <div className="cards-main row">
+                                <div className="col-lg-4">
+                                    <div className="paper-card">
+                                        <IoIosHeartEmpty className='heart-icon' />
+                                        <img src="/Images/Group 1000004522.png" alt="" className='paper-image' />
+                                        <div className="lesson-container">
+                                            <div className="lesson-icon">
+                                            <FaFileInvoiceDollar className='icon' />
+                                            </div>
+                                            <span>7 Lessons</span>
+                                        </div>
+                                        <h1 className="paper-title">Common Paper</h1>
+                                        <p className="paper-description">Budget Analysis examines income, expenses, and goals for efficient resource allocation.</p>
+                                        <button>Learn Now</button>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            </>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/SideNav/SideNave.jsx
+++ src/components/SideNav/SideNave.jsx
@@ -1,6 +1,12 @@
+import { useEffect, useState } from 'react'
 import './SideNav.scss'
 
 const SideNave = () => {
+    const [activePath,setActivePath]=useState('')
+    useEffect(()=>{
+        setActivePath(window.location.pathname)
+    },[activePath])
+    
   return (
     <div className='SideNavMainWrapper'>
         <div className="desktopSideNavWrapper">
@@ -12,11 +18,11 @@ const SideNave = () => {
                     <img src="/Images/Frame 1261153187.png" alt="" />
                 </div>
                 <div className="nav-items">
-                    <div className='nav-item'><img src="/Images/Group 1000004529.png" alt="" /></div>
-                    <div className='nav-item'><img src="/Images/Group 1000004528.png" alt="" /></div>
-                    <div className='nav-item'><img src="/Images/Vector (1).png" alt="" /></div>
-                    <div className='nav-item'><img src="/Images/Group 1000004496.png" alt="" /></div>
-                    <div className='nav-item'><img src="/Images/Group 1000004528 (1).png" alt="" /></div>
+                    <div className={`nav-item ${activePath=='/'?'active':''}`}><img src="/Images/Group 1000004529.png" alt="" /></div>
+                    <div className={`nav-item`}><img src="/Images/Group 1000004528.png" alt="" /></div>
+                    <div className={`nav-item`}><img src="/Images/Vector (1).png" alt="" /></div>
+                    <div className={`nav-item`}><img src="/Images/Group 1000004496.png" alt="" /></div>
+                    <div className={`nav-item`}><img src="/Images/Group 1000004528 (1).png" alt="" /></div>
                 </div>
             </div>
         </div>
